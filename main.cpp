5a92b32dded7efc80d7c058e3545ea7745616182
vitaliuchik <vitaliypapka@gmail.com>
2020-02-14 12:00:25 +0200

    two convert methods

--- main.cpp
+++ main.cpp
@@ -2,32 +2,61 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <sstream>
 
-void file_read(std::string path);
+
+std::vector<int> file_read(const std::string& path);
+void sstream_convert(std::vector<int> *int_v, std::string *str_arr);
+void to_string_convert(std::vector<int> *int_v, std::string *str_arr);
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    file_read("nums.txt");
+    std::vector<int> int_v;
+    int_v = file_read("nums.txt");
+    int length = int_v.size();
+    auto* str_arr = new std::string[length];
+
+//    sstream_convert(&int_v, str_arr)
+    to_string_convert(&int_v, str_arr);
+
+    std::cout << str_arr[0] << str_arr[1] << str_arr[2] << str_arr[3] << std::endl;
+
     return 0;
 }
 
-void file_read(std::string path) {
+std::vector<int> file_read(const std::string& path) {
     std::vector<int> v;
-    std::ifstream  file(path);
+    std::ifstream file(path);
 
     if(!file){
         std::cerr << "Error opening file" << std::endl;
-        return;
+        return v;
     }
 
     int x;
-    int sum = 0, n = 0;
-    while( file >> x ){
-        sum += x;
-        ++n;
+    while( file >> x )
         v.push_back(x);
-    }
 
-    std::cout << "Sum = " << sum << std::endl << "N = " << n << std::endl << "Average = " << sum/n << std::endl;
-
+    return v;
 }
+
+void sstream_convert(std::vector<int> *int_v, std::string *str_arr){
+    int i = 0;
+    std::stringstream ss;
+    for (int num: *int_v) {
+        ss << num;
+        ss >> str_arr[i];
+        ss.clear();
+        i++;
+    }
+}
+
+void to_string_convert(std::vector<int> *int_v, std::string *str_arr){
+    int i = 0;
+    for (int num: *int_v) {
+        str_arr[i] = std::to_string(num);
+        i++;
+    }
+}
+
+
