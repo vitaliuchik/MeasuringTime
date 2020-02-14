230b85b73ee96cfe9146248433d3fcfab0861d12
Sotnyk <danylos2001@gmail.com>
2020-02-14 10:58:41 +0200

    reading file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+void file_read(std::string path);
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    file_read("nums.txt");
     return 0;
+}
+
+void file_read(std::string path) {
+    std::vector<int> v;
+    std::ifstream  file(path);
+
+    if(!file){
+        std::cerr << "Error opening file" << std::endl;
+        return;
+    }
+
+    int x;
+    int sum = 0, n = 0;
+    while( file >> x ){
+        sum += x;
+        ++n;
+        v.push_back(x);
+    }
+
+    std::cout << "Sum = " << sum << std::endl << "N = " << n << std::endl << "Average = " << sum/n << std::endl;
+
 }
